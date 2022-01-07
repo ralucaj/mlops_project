2933a2aae483b8252e79c5bfee283ab24488fc75
Jacopo Gaetani <jacopus.g@gmail.com>
2022-01-07 15:11:03 +0100

    Added cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository for the project of the course "02476 - Machine Learning Operations"',
+    author='Raluca Jalaboi, Efstathios Alex Voinas, Jacopo Gaetani',
+    license='',
+)
